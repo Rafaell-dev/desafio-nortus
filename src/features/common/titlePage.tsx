1f13a1cc9a21fc0e582d4e7e5eeecba57d26afb7
rafaelldev <rafaelaraujo.contact@gmail.com>
2026-02-14 22:16:52 -0300

    style: fix title page max width

--- src/features/common/titlePage.tsx
+++ src/features/common/titlePage.tsx
@@ -5,7 +5,9 @@ interface TitlePageProps {
 export function TitlePage({ title }: TitlePageProps) {
   return (
     <div className="bg-dark-surface w-full px-8 py-6">
-      <h1 className="text-xl font-semibold text-white">{title}</h1>
+      <div className="mx-auto max-w-7xl">
+        <h1 className="text-xl font-semibold text-white">{title}</h1>
+      </div>
     </div>
   );
 }
