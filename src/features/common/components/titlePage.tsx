f694b578fee63d7945a320ad7c01b34f049529e6
rafaelldev <rafaelaraujo.contact@gmail.com>
2026-02-16 23:29:39 -0300

    feat: add new prop action to titlepage

--- src/features/common/components/titlePage.tsx
+++ src/features/common/components/titlePage.tsx
@@ -1,12 +1,14 @@
 interface TitlePageProps {
   title: string;
+  action?: React.ReactNode;
 }
 
-export function TitlePage({ title }: TitlePageProps) {
+export function TitlePage({ title, action }: TitlePageProps) {
   return (
     <div className="bg-dark-surface w-full px-8 py-6">
-      <div className="mx-auto max-w-7xl">
+      <div className="mx-auto flex max-w-7xl items-center justify-between">
         <h1 className="text-xl font-semibold text-white">{title}</h1>
+        {action}
       </div>
     </div>
   );
